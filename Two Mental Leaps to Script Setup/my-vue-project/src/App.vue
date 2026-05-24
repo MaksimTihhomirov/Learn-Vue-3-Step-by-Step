2ab27335c0f433897b2919783ad6c88ed14809ea
Maksim <maksim.tihhomirov@techno.ee>
2026-05-24 18:50:08 +0300

    two mental leaps to script setup

---
+++ Two Mental Leaps to Script Setup/my-vue-project/src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+
+function test() {
+  alert('doing it now')
+}
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+
+    <h3>Hello, World!</h3>
+
+    <button @click="test">
+      Click Me
+    </button>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+main {
+  padding-top: 20px;
+}
+</style>
