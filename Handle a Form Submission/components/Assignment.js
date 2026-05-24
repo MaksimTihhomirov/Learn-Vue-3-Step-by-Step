9ddd055c2c82e11f213557490a60a006fd35b0b7
Maksim <maksim.tihhomirov@techno.ee>
2026-05-24 17:40:28 +0300

    Handle a Form Submission

---
+++ Handle a Form Submission/components/Assignment.js
@@ -0,0 +1,18 @@
+export default {
+    props: {
+        assignment: Object
+    },
+
+    template: `
+        <li>
+            <label>
+                {{ assignment.name }}
+
+                <input
+                    type="checkbox"
+                    v-model="assignment.complete"
+                >
+            </label>
+        </li>
+    `
+}
